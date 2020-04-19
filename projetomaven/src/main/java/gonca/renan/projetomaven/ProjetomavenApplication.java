613771f1104712de3cceeef2b8619d4e725d6871
Renan <renan.g200@gmail.com>
2020-04-19 16:38:12 -0300

    Exemplo Maven

---
+++ projetomaven/src/main/java/gonca/renan/projetomaven/ProjetomavenApplication.java
@@ -0,0 +1,21 @@
+package gonca.renan.projetomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Renan
+ * @since 19/4/2020
+ * @version 1.0
+ */
+
+@SpringBootApplication
+public class ProjetomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomavenApplication.class, args);
+
+		System.out.println("Hello World SringBOOT com Maven");
+	}
+
+}
---
+++ projetomaven/src/test/java/gonca/renan/projetomaven/ProjetomavenApplicationTests.java
@@ -0,0 +1,13 @@
+package gonca.renan.projetomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
